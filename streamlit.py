730151cf4b003a501bda2c71661e1ed5a59095b1
pmcebepi-max <pmcebepi@gmail.com>
2025-08-17 16:13:56 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -25,11 +25,11 @@ st.dataframe(df_filtrado)
 def gerar_pdf(df, polo, data):
     pdf = FPDF()
     pdf.add_page()
-    pdf.set_font("Arial", "B", 14)  # título um pouco menor
+    pdf.set_font("Arial", "B", 12)  # título um pouco menor
     pdf.cell(0, 10, f"Lista de Presença - {polo} - {data}", ln=True, align="C")
     pdf.ln(5)
     
-    pdf.set_font("Arial", "", 10)  # fonte menor para tabela
+    pdf.set_font("Arial", "", 8)  # fonte menor para tabela
     colunas = df.columns.tolist()
     col_width = pdf.w / (len(colunas)+1)
     
